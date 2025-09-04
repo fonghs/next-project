4cca5e7956e4ae3a19dc90a4e2995a4e609a6fc3
何淑芳 <fonghs95@emct.com.tw>
2025-09-04 15:20:23 +0800

    web config 調整

--- src/app/page.js
+++ src/app/page.js
@@ -29,7 +29,7 @@ export default function Home() {
             .
           </li>
           <li className="tracking-[-.01em]">
-            Save and see your changes instantly.（123）
+            Save and see your changes instantly.
           </li>
         </ol>
 
